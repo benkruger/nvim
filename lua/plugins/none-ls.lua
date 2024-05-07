e72ce0a8e3195957552cdf6cc12e0797ef860e8d
Ben Kruger <ben@benkruger.com>
2024-05-07 11:46:07 -0700

    rubocop formatting and linting working

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -4,9 +4,13 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
-				--null_ls.builtins.completion.spell,
-				-- require("none-ls.diagnostics.eslint"), -- requires none-ls-extras.nvim
+        -- Lua
+        null_ls.builtins.formatting.stylua,
+        -- Ruby
+        null_ls.builtins.diagnostics.rubocop,
+        null_ls.builtins.formatting.rubocop,
+        -- SQL
+        null_ls.builtins.formatting.sql_formatter,
 			},
 		})
 		vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
